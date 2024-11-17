de710686e9c4d0ebad7d9a39d89f5e4d938f39c8
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-16 16:20:10 -0800

    Catching exceptions in service class

--- src/main/java/com/danielagapov/spawn/Services/IUserService.java
+++ src/main/java/com/danielagapov/spawn/Services/IUserService.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface IUserService {
     public List<User> getAllUsers();
-    public Optional<User> getUserById(Long id);
+    public User getUserById(Long id);
     public User saveUser(User user);
 }
--- src/main/java/com/danielagapov/spawn/Services/UserService.java
+++ src/main/java/com/danielagapov/spawn/Services/UserService.java
@@ -1,8 +1,11 @@
 package com.danielagapov.spawn.Services;
 
+import com.danielagapov.spawn.Exceptions.User.UserNotFoundException;
+import com.danielagapov.spawn.Exceptions.User.UserSaveException;
 import com.danielagapov.spawn.Models.User;
 import com.danielagapov.spawn.Repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,14 +21,23 @@ public class UserService implements IUserService{
     }
 
     public List<User> getAllUsers() {
-        return repository.findAll();
+        try {
+            return repository.findAll();
+        } catch (DataAccessException e) {
+            throw new RuntimeException("Error retrieving users", e);
+        }
     }
 
-    public Optional<User> getUserById(Long id) {
-        return repository.findById(id);
+    public User getUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 
     public User saveUser(User user) {
-        return repository.save(user);
+        try {
+            return repository.save(user);
+        } catch (DataAccessException e) {
+            throw new UserSaveException("Failed to save user: " + e.getMessage());
+        }
     }
 }
