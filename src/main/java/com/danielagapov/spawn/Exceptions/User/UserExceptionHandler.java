4b964877639512dd096f465c8d3de6e00ced0bce
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-16 16:27:25 -0800

    Handling user exceptions by returning custom HTTP responses to front-end

---
+++ src/main/java/com/danielagapov/spawn/Exceptions/User/UserExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.danielagapov.spawn.Exceptions.User;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserSaveException.class)
+    public ResponseEntity<String> handleUserSaveException(UserSaveException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
