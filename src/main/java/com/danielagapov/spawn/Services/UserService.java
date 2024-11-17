cdcbcf5bf141fa4afe8629a55cfc56b77f4d5104
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-16 16:11:10 -0800

    Created `UserService`, to be between the controller and repo

---
+++ src/main/java/com/danielagapov/spawn/Services/UserService.java
@@ -0,0 +1,31 @@
+package com.danielagapov.spawn.Services;
+
+import com.danielagapov.spawn.Models.User;
+import com.danielagapov.spawn.Repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final IUserRepository repository;
+
+    @Autowired
+    public UserService(IUserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return repository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return repository.save(user);
+    }
+}
