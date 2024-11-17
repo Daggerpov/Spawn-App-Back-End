b1e284015c5a5efe22c658e8684bede369322471
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-16 21:13:25 -0800

    Made event service, the same as with users

---
+++ src/main/java/com/danielagapov/spawn/Services/Event/EventService.java
@@ -0,0 +1,52 @@
+package com.danielagapov.spawn.Services.Event;
+
+import com.danielagapov.spawn.Exceptions.Base.BasesNotFoundException;
+import com.danielagapov.spawn.Exceptions.Base.BaseNotFoundException;
+import com.danielagapov.spawn.Exceptions.Base.BaseSaveException;
+import com.danielagapov.spawn.Models.Event;
+import com.danielagapov.spawn.Repositories.IEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService implements IEventService {
+    private final IEventRepository repository;
+
+    @Autowired
+    public EventService(IEventRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Event> getAllEvents() {
+        try {
+            return repository.findAll();
+        } catch (DataAccessException e) {
+            throw new BasesNotFoundException();
+        }
+    }
+
+    public Event getEventById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new BaseNotFoundException(id));
+    }
+
+    public List<Event> getEventsByTagId(Long tagId) {
+        // TODO: change this logic later, once tags are setup.
+        try {
+            return repository.findAll();
+        } catch (DataAccessException e) {
+            throw new RuntimeException("Error retrieving events", e);
+        }
+    }
+
+    public Event saveEvent(Event event) {
+        try {
+            return repository.save(event);
+        } catch (DataAccessException e) {
+            throw new BaseSaveException("Failed to save event: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/danielagapov/spawn/Services/Event/IEventService.java
@@ -0,0 +1,11 @@
+package com.danielagapov.spawn.Services.Event;
+
+import java.util.List;
+import com.danielagapov.spawn.Models.Event;
+
+public interface IEventService {
+    public List<Event> getAllEvents();
+    public Event getEventById(Long id);
+    public List<Event> getEventsByTagId(Long tagId);
+    public Event saveEvent(Event event);
+}
