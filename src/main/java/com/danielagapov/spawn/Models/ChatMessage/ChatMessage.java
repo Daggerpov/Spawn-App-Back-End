b1f022be1506a0a2c55c4c911c681a238a673d53
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-20 12:32:54 -0800

    Leaving out `@Column` annotations from this PR (will add later, in separate one)
    This reverts commit 4cc7dddc37cf8bbff5549aaeff1b27dbd433ba00.

--- src/main/java/com/danielagapov/spawn/Models/ChatMessage/ChatMessage.java
+++ src/main/java/com/danielagapov/spawn/Models/ChatMessage/ChatMessage.java
@@ -19,24 +19,11 @@ import java.util.UUID;
 // these two annotations are in place of writing out getters and setters manually (for readability):
 @Getter
 @Setter
-@Table(name = "ChatMessage")
 public class ChatMessage implements Serializable {
-        @Id
-        @GeneratedValue
-        @Column(name = "Id", nullable = false, updatable = false)
-        private UUID id;
-
-        @Column(name = "Timestamp", nullable = false)
+        private @Id
+        @GeneratedValue Long id;
         private Instant timestamp;
-
-        @Column(name = "Content", nullable = false)
+        private Long userSenderId;
         private String content;
-
-        @ManyToOne
-        @JoinColumn(name = "User", referencedColumnName = "id")
-        private User user;
-
-        @ManyToOne
-        @JoinColumn(name = "EventId", referencedColumnName = "id")
-        private Event event;
+        private Long eventId;
 }
