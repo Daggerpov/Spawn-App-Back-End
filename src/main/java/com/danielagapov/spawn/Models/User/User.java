3bbf1c2f5a530cf28584b12b5ab0f47058573544
Daniel Agapov <daniel.agapov@cambioearth.com>
2024-11-20 12:25:19 -0800

    Utilizing lombok to remove boilerplate constructors, getters, and setters in entity classes

--- src/main/java/com/danielagapov/spawn/Models/User/User.java
+++ src/main/java/com/danielagapov/spawn/Models/User/User.java
@@ -3,9 +3,20 @@ package com.danielagapov.spawn.Models.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
 @Entity
+// these two annotations are in place of writing out constructors manually (for readability)
+@NoArgsConstructor
+@AllArgsConstructor
+// these two annotations are in place of writing out getters and setters manually (for readability):
+@Getter
+@Setter
 public class User  implements Serializable {
         private @Id
         @GeneratedValue Long id;
@@ -14,62 +25,4 @@ public class User  implements Serializable {
         private String lastName;
         private String bio;
         private String profilePicture; // TODO: reconsider data type later
-
-        public User() {}
-
-        public User(Long id, String username, String firstName, String lastName, String bio) {
-                this.id = id;
-                this.username = username;
-                this.firstName = firstName;
-                this.lastName = lastName;
-                this.bio = bio;
-        }
-
-        public Long getId() {
-                return id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-        }
-
-        public String getUsername() {
-                return username;
-        }
-
-        public void setUsername(String username) {
-                this.username = username;
-        }
-
-        public String getFirstName() {
-                return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-                this.firstName = firstName;
-        }
-
-        public String getLastName() {
-                return lastName;
-        }
-
-        public void setLastName(String lastName) {
-                this.lastName = lastName;
-        }
-
-        public String getBio() {
-                return bio;
-        }
-
-        public void setBio(String bio) {
-                this.bio = bio;
-        }
-
-        public String getProfilePicture() {
-                return profilePicture;
-        }
-
-        public void setProfilePicture(String profilePicture) {
-                this.profilePicture = profilePicture;
-        }
 }
