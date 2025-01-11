30717dd3745df63a4ed48220072b7f00254df975
shane <shane.mander31@gmail.com>
2025-01-11 14:54:41 -0800

    commented out oauth stuff

--- src/main/java/com/danielagapov/spawn/Services/OAuth/IOAuthService.java
+++ src/main/java/com/danielagapov/spawn/Services/OAuth/IOAuthService.java
@@ -1,9 +1,9 @@
 package com.danielagapov.spawn.Services.OAuth;
 
 import com.danielagapov.spawn.DTOs.UserDTO;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-
-public interface IOAuthService {
-
-    UserDTO verifyUser(OAuth2User user);
-}
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//
+//public interface IOAuthService {
+//
+//    UserDTO verifyUser(OAuth2User user);
+//}
