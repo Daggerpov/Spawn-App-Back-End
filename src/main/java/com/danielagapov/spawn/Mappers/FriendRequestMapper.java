fc5ce1e12438de32d77c93d524312ff5d8212767
Daggerpov <danielagapov1@gmail.com>
2025-01-19 00:56:22 -0800

    updated logic to be more explicit and consistent in toentitylist

--- src/main/java/com/danielagapov/spawn/Mappers/FriendRequestMapper.java
+++ src/main/java/com/danielagapov/spawn/Mappers/FriendRequestMapper.java
@@ -31,17 +31,17 @@ public class FriendRequestMapper {
                 .collect(Collectors.toList());
     }
 
-    public static List<FriendRequest> toEntityList(List<FriendRequestDTO> friendRequestDTOList, List<User> users) {
+    public static List<FriendRequest> toEntityList(List<FriendRequestDTO> friendRequestDTOList, List<User> userSenders, List<User> userReceivers) {
         return friendRequestDTOList.stream()
                 .map(dto -> {
-                    // Find sender user by matching senderId
-                    User sender = users.stream()
+                    // Find sender user by matching senderId from userSenders list
+                    User sender = userSenders.stream()
                             .filter(user -> user.getId().equals(dto.senderUserId()))
                             .findFirst()
                             .orElse(null);
 
-                    // Find receiver user by matching receiverId
-                    User receiver = users.stream()
+                    // Find receiver user by matching receiverId from userReceivers list
+                    User receiver = userReceivers.stream()
                             .filter(user -> user.getId().equals(dto.receiverUserId()))
                             .findFirst()
                             .orElse(null);
@@ -50,5 +50,6 @@ public class FriendRequestMapper {
                 })
                 .collect(Collectors.toList());
     }
+
 
 }
