b46e57b2c0a21c549e15a31bc465dea6a53ca47c
Daggerpov <danielagapov1@gmail.com>
2025-01-23 16:48:48 -0800

    Added `@Modifying` to delete query

--- src/main/java/com/danielagapov/spawn/Repositories/IUserFriendTagRepository.java
+++ src/main/java/com/danielagapov/spawn/Repositories/IUserFriendTagRepository.java
@@ -2,6 +2,7 @@ package com.danielagapov.spawn.Repositories;
 
 import com.danielagapov.spawn.Models.UserFriendTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +15,7 @@ public interface IUserFriendTagRepository extends JpaRepository<UserFriendTag, U
     @Query("SELECT uft.friend.id FROM UserFriendTag uft WHERE uft.friendTag.id = :tagId")
     List<UUID> findFriendIdsByTagId(@Param("tagId") UUID tagId);
 
+    @Modifying
     @Query("DELETE FROM UserFriendTag uft WHERE uft.friendTag.id = :tagId AND uft.friend.id = :friendId")
     void deleteByFriendTagIdAndUserId(@Param("tagId") UUID tagId, @Param("friendId") UUID friendId);
 }
