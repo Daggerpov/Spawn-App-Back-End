d394246754bed3a1a0b05432aea5fa39098aef3d
shane <shane.mander31@gmail.com>
2025-02-01 22:38:07 -0800

    s3 documentation

--- src/main/java/com/danielagapov/spawn/Services/S3/IS3Service.java
+++ src/main/java/com/danielagapov/spawn/Services/S3/IS3Service.java
@@ -6,9 +6,34 @@ import com.danielagapov.spawn.DTOs.UserDTO;
 import java.util.UUID;
 
 public interface IS3Service {
+    /**
+     * Given key, puts the given file into the spawn s3 bucket
+     * @param file => byte array representation of a file (usually a .jpeg)
+     * @param key => string
+     * @return a cdn url string to access the newly added object
+     */
     String putObjectWithKey(byte[] file, String key);
+
+    /**
+     * Given a user id, deletes a user's profile picture
+     * @param userId => id of user whose pfp is to delete
+     */
     void deleteObjectByUserId(UUID userId);
+
+    /**
+     * Puts the file into an s3 bucket
+     * @param file => byte array representation of a file (usually a .jpeg)
+     * @return a cdn url string to access the newly added object
+     */
     String putObject(byte[] file);
+
+    /**
+     * Puts the file into an s3 bucket and attaches the cdn url string to the given user dto.
+     * If file is null, a default url string is attached
+     * @param file => byte array representation of a file (usually a .jpeg)
+     * @param user => user to attach url string with
+     * @return UserDTO with a set profilePictureUrlString
+     */
     UserDTO putProfilePictureWithUser(byte[] file, UserDTO user);
     UserDTO updateProfilePicture(byte[] file, UUID userId);
 }
