7353b4b4141a4d9ae9a023654b57e8fe87ed9107
shane <shane.mander31@gmail.com>
2025-02-04 23:12:12 -0800

    jwt filter

--- src/main/java/com/danielagapov/spawn/Services/JWT/IJWTService.java
+++ src/main/java/com/danielagapov/spawn/Services/JWT/IJWTService.java
@@ -1,5 +1,8 @@
 package com.danielagapov.spawn.Services.JWT;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface IJWTService {
     String extractUsername(String token);
+    boolean isValidToken(String token, UserDetails userDetails);
 }
