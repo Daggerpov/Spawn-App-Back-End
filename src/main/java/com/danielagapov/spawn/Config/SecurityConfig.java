1a5da10837eaa2b8eba439b287481809801ae864
shane <shane.mander31@gmail.com>
2025-02-12 21:41:20 -0800

    permit beta access

--- src/main/java/com/danielagapov/spawn/Config/SecurityConfig.java
+++ src/main/java/com/danielagapov/spawn/Config/SecurityConfig.java
@@ -40,9 +40,83 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-                .oauth2Login(oauth2 -> {
-                    oauth2.successHandler((request, response, authentication) -> response.sendRedirect("/api/v1/oauth/google/sign-in"));
-                });
+                // Endpoints can be made unsecured by specifying it with requestMatchers() below and permitting
+                // that be accessed without authentication with permitAll().
+                // Below, the auth and oauth endpoints are unsecured
+                .authorizeHttpRequests(authorize -> authorize
+                                //        .requestMatchers("/api/v1/auth/**", "/api/v1/betaAccessSignUp/**").permitAll()
+                                .anyRequest().permitAll()
+                        //.anyRequest()
+                        //.authenticated() // Comment this out if wanting to unsecure endpoints for development purposes
+                )
+                // When authenticating a request fails, status code 401 (unauthorized) is returned
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                // 'Stateless' session management means Spring will not create and store any session state on the server
+                // Each request is treated as 'new' and thus requires authentication (a JWT) to access secured endpoints
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
         return http.build();
+    }
+
+
+    /**
+     * Provides the default AuthenticationManager bean.
+     * <p>
+     * The AuthenticationManager is responsible for processing authentication requests.
+     * It delegates authentication to the configured AuthenticationProvider.
+     *
+     * @param config The AuthenticationConfiguration provided by Spring Security.
+     * @return The AuthenticationManager instance.
+     * @throws Exception If an error occurs while retrieving the authentication manager.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager(); // use default auth manager
+    }
+
+    /**
+     * Configures the authentication provider for the application.
+     * <p>
+     * DaoAuthenticationProvider is used to authenticate users by retrieving user details
+     * from the database and validating their credentials.
+     *
+     * @return A configured DaoAuthenticationProvider instance.
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userInfoService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    /**
+     * Provides a password encoder bean.
+     * <p>
+     * BCryptPasswordEncoder is a strong and secure password hashing algorithm
+     * that automatically manages salting for added security.
+     *
+     * @return An instance of BCryptPasswordEncoder.
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Provides a UserDetailsService bean.
+     * <p>
+     * This service is responsible for loading user details from the database
+     * based on the username during authentication.
+     *
+     * @return An instance of UserDetailsService, implemented by UserInfoService.
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userInfoService;
     }
 }
