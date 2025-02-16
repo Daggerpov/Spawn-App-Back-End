e58782406947d5df67e27535e000c8408afcdfbb
shane <shane.mander31@gmail.com>
2025-02-15 16:52:47 -0800

    on user delete cascade

--- src/main/java/com/danielagapov/spawn/Models/ChatMessageLikes.java
+++ src/main/java/com/danielagapov/spawn/Models/ChatMessageLikes.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.io.Serializable;
 
@@ -27,5 +29,6 @@ public class ChatMessageLikes implements Serializable {
     @ManyToOne
     @MapsId("userId")
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 }
