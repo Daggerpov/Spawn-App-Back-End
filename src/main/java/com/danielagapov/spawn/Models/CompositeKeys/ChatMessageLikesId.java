475a4fceb888fc105ee1d9044c2aa01aaa80c222
Daggerpov <danielagapov1@gmail.com>
2025-02-18 02:24:45 -0800

    Fixed composite ids by implementing `equals()` and `hashCode()`

--- src/main/java/com/danielagapov/spawn/Models/CompositeKeys/ChatMessageLikesId.java
+++ src/main/java/com/danielagapov/spawn/Models/CompositeKeys/ChatMessageLikesId.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Embeddable
@@ -21,4 +22,17 @@ public class ChatMessageLikesId implements Serializable {
 
     @Column(name = "user_id", nullable = false)
     private UUID userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChatMessageLikesId that)) return false;
+        return Objects.equals(chatMessageId, that.chatMessageId) &&
+                Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chatMessageId, userId);
+    }
 }
