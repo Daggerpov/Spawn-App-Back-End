e69c8a78e6fec8f1777e5f52f686d24551f928ed
shane <shane.mander31@gmail.com>
2025-02-21 00:03:56 -0800

    rename env var

--- src/main/java/com/danielagapov/spawn/Config/S3Config.java
+++ src/main/java/com/danielagapov/spawn/Config/S3Config.java
@@ -13,8 +13,8 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
-        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+        String accessKey = System.getenv("AWS_ACCESS_KEY_ID_");
+        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY_");
 
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         if (accessKey == null) accessKey = dotenv.get("AWS_ACCESS_KEY_ID");
