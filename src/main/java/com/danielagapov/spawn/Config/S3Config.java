67ea71289c82baba78e26a380a7a39db6ef69b44
shane <shane.mander31@gmail.com>
2025-02-20 22:22:10 -0800

    fix s3 credentials

--- src/main/java/com/danielagapov/spawn/Config/S3Config.java
+++ src/main/java/com/danielagapov/spawn/Config/S3Config.java
@@ -1,7 +1,10 @@
 package com.danielagapov.spawn.Config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -10,8 +13,16 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
+        Dotenv dotenv = Dotenv.load();
+        String accessKey = dotenv.get("AWS_ACCESS_KEY_ID");
+        String secretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
+
         return S3Client.builder()
                 .region(Region.US_WEST_2)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
