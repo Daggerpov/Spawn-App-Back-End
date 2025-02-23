c95d384f4472a7bdf7aaf736ebf3d02e491b9700
Seabert Yuan <seabert.s.yuan23z@gmail.com>
2025-02-22 18:35:59 -0800

    overrode equals for SearchUserResult

--- src/main/java/com/danielagapov/spawn/Utils/SearchedUserResult.java
+++ src/main/java/com/danielagapov/spawn/Utils/SearchedUserResult.java
@@ -7,8 +7,19 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+// A special instance of Triple
 public class SearchedUserResult extends Triple<List<FullFriendRequestDTO>, List<RecommendedFriendUserDTO>, List<FullFriendUserDTO>> {
     public SearchedUserResult(List<FullFriendRequestDTO> friendRequestDTOS, List<RecommendedFriendUserDTO> recommendedFriendUserDTOS, List<FullFriendUserDTO> fullFriendUserDTOS) {
         super(friendRequestDTOS, recommendedFriendUserDTOS, fullFriendUserDTOS);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof SearchedUserResult))
+            return false;
+        SearchedUserResult other = (SearchedUserResult)obj;
+        return other.first().equals(this.first()) && other.second().equals(this.second()) && other.third().equals(this.third());
     }
 }
