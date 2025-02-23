be346d86e054db6e554f5421d1cce53ae4dfcb5a
Seabert Yuan <seabert.s.yuan23z@gmail.com>
2025-02-23 11:45:42 -0800

    updated AbstractFriendTagDTO to fix #209

--- src/main/java/com/danielagapov/spawn/DTOs/FriendTag/AbstractFriendTagDTO.java
+++ src/main/java/com/danielagapov/spawn/DTOs/FriendTag/AbstractFriendTagDTO.java
@@ -1,18 +1,20 @@
 package com.danielagapov.spawn.DTOs.FriendTag;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 public abstract class AbstractFriendTagDTO implements Serializable {
     UUID id;
     String displayName;
     String colorHexCode;
+    @Getter(AccessLevel.NONE)
     boolean isEveryone;
+
+    public boolean isEveryone() {
+        return isEveryone;
+    }
 }
