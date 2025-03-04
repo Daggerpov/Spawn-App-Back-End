9e23f8e416d9777edfb530d300f0fa633db5d41a
Daggerpov <danielagapov1@gmail.com>
2025-03-03 16:19:00 -0800

    New `BaseNotFoundException` with just entity type

--- src/main/java/com/danielagapov/spawn/Exceptions/Base/BaseNotFoundException.java
+++ src/main/java/com/danielagapov/spawn/Exceptions/Base/BaseNotFoundException.java
@@ -5,10 +5,14 @@ import com.danielagapov.spawn.Enums.EntityType;
 import java.util.UUID;
 
 public class BaseNotFoundException extends RuntimeException {
+    public BaseNotFoundException(EntityType et) {
+        super(et +  " entity not found");
+    }
     public BaseNotFoundException(EntityType et, UUID id) {
         super(et +  " entity not found with ID: " + id);
     }
     public BaseNotFoundException(EntityType et, String username) {
         super(et +  " entity not found with username: " + username);
     }
+
 }
