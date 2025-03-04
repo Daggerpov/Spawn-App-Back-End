0673fd61ca555c9d44228527040dfb3be110009b
shane <shane.mander31@gmail.com>
2025-03-03 21:04:59 -0800

    Remove PotentialFriendUserDTO and replace with BaseUserDTO

--- src/main/java/com/danielagapov/spawn/DTOs/User/FriendUser/RecommendedFriendUserDTO.java
+++ src/main/java/com/danielagapov/spawn/DTOs/User/FriendUser/RecommendedFriendUserDTO.java
@@ -1,5 +1,6 @@
 package com.danielagapov.spawn.DTOs.User.FriendUser;
 
+import com.danielagapov.spawn.DTOs.User.BaseUserDTO;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,9 @@ import java.util.UUID;
 
 @Getter
 @Setter
-public class RecommendedFriendUserDTO extends AbstractFriendUserDTO implements Serializable {
+public class RecommendedFriendUserDTO extends BaseUserDTO implements Serializable {
     int mutualFriendCount;
-    
+
     public RecommendedFriendUserDTO(UUID id, String firstName, String lastName, String email, String username, String bio, String profilePicture, int mutualFriendCount) {
         super(id, firstName, lastName, email, username, bio, profilePicture);
         this.mutualFriendCount = mutualFriendCount;
