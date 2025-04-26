ec7c9b45cf667382e5b035c477904ce047fd434a
Daniel Agapov <danielagapov1@gmail.com>
2025-04-26 15:43:55 -0700

    added to user friend tag

--- scripts/initialize_friend_tag_last_updated.sql
+++ scripts/initialize_friend_tag_last_updated.sql
@@ -1,5 +1,5 @@
 ALTER TABLE friend_tag
-ADD COLUMN last_modified TIMESTAMP;
+ADD COLUMN last_updated TIMESTAMP;
 -- Update existing records to have a default value
 UPDATE friend_tag
-SET last_modified = NOW();
+SET last_updated = NOW();
