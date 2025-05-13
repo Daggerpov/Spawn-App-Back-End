d1f25ca58f4e7a3cc046415ae8179c561c0ce37f
Shane Mander <shane.mander31@gmail.com>
2025-05-12 22:44:15 -0700

    Add getter

--- src/main/java/com/danielagapov/spawn/DTOs/CalendarActivityDTO.java
+++ src/main/java/com/danielagapov/spawn/DTOs/CalendarActivityDTO.java
@@ -1,10 +1,7 @@
 package com.danielagapov.spawn.DTOs;
 
 import com.danielagapov.spawn.Enums.EventCategory;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -12,10 +9,11 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class CalendarActivityDTO {
     private UUID id;
     private String date; // ISO format: YYYY-MM-DD
-    private EventCategory eventCategory; 
+    private EventCategory eventCategory;
     private String icon; // Icon for the calendar event (emoji)
     private UUID eventId; // Optional, if the activity is linked to a spawn event
 } 
