c54ccc997f0545e6bcc167599337ff2aa97668cf
Daggerpov <danielagapov1@gmail.com>
2025-06-03 02:02:21 -0700

    Update S3Config.java

--- src/main/java/com/danielagapov/spawn/Config/S3Config.java
+++ src/main/java/com/danielagapov/spawn/Config/S3Config.java
@@ -3,12 +3,14 @@ package com.danielagapov.spawn.Config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("!test")
 public class S3Config {
 
     @Bean
