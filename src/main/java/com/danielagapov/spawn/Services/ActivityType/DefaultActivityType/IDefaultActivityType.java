a80660bf86204e1123a47c5698dfedd9ef3fba15
ShaneMander <shane.mander31@gmail.com>
2025-06-13 21:01:18 -0700

    Remove IDefaultActivityType

--- src/main/java/com/danielagapov/spawn/Services/ActivityType/DefaultActivityType/IDefaultActivityType.java
+++
@@ -1,9 +0,0 @@
-package com.danielagapov.spawn.Services.ActivityType.DefaultActivityType;
-
-import com.danielagapov.spawn.Models.ActivityType;
-import com.danielagapov.spawn.Models.User.User;
-
-public interface IDefaultActivityType {
-
-    ActivityType getDefaultActivityType(User user);
-}
