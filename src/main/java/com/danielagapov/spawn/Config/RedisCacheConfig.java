c8e29cb0fc6022a1f3b9a4bfee0bdfd301955b44
Daggerpov <danielagapov1@gmail.com>
2025-07-02 18:27:06 -0700

    fix repo tests redis config

--- src/main/java/com/danielagapov/spawn/Config/RedisCacheConfig.java
+++ src/main/java/com/danielagapov/spawn/Config/RedisCacheConfig.java
@@ -1,10 +1,13 @@
 package com.danielagapov.spawn.Config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,6 +16,9 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
+@ConditionalOnClass(RedisConnectionFactory.class)
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis", matchIfMissing = false)
+@Profile("!test") // Don't load this configuration in test profile
 public class RedisCacheConfig {
 
     @Bean
