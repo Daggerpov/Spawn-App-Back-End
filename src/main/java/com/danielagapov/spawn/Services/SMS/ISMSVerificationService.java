7d6753a7183f4f89ee3aab4425d76cd907e60c7b
ShaneMander <shane.mander31@gmail.com>
2025-07-11 23:09:58 -0700

    Delete SMS stuff

--- src/main/java/com/danielagapov/spawn/Services/SMS/ISMSVerificationService.java
+++
@@ -1,8 +0,0 @@
-package com.danielagapov.spawn.Services.SMS;
-
-public interface ISMSVerificationService {
-
-    void sendSMSVerification(String phoneNumber);
-
-    boolean checkSMSVerification(String phoneNumber, String verificationCode);
-}
