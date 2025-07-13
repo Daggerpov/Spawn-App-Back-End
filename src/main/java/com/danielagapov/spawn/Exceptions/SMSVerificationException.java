68669ae97c5bb4175f7156b8158121ad6ac5f565
ShaneMander <shane.mander31@gmail.com>
2025-07-12 19:58:39 -0700

    Delete old Phone Number verification related files

--- src/main/java/com/danielagapov/spawn/Exceptions/SMSVerificationException.java
+++
@@ -1,7 +0,0 @@
-package com.danielagapov.spawn.Exceptions;
-
-public class SMSVerificationException extends RuntimeException {
-    public SMSVerificationException(String message) {
-        super(message);
-    }
-}
