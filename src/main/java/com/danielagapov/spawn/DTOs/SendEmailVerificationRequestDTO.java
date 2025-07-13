334553fdbcc2cd6639730954477fa9096135f3ca
ShaneMander <shane.mander31@gmail.com>
2025-07-12 20:31:31 -0700

    Add annotation to dto

--- src/main/java/com/danielagapov/spawn/DTOs/SendEmailVerificationRequestDTO.java
+++ src/main/java/com/danielagapov/spawn/DTOs/SendEmailVerificationRequestDTO.java
@@ -1,14 +1,12 @@
 package com.danielagapov.spawn.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
 
 @Getter
+@AllArgsConstructor
 public class SendEmailVerificationRequestDTO implements Serializable {
     private final String email;
-
-    public SendEmailVerificationRequestDTO(String email) {
-        this.email = email;
-    }
 } 
