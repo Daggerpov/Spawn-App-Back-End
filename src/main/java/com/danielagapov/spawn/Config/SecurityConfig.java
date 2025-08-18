828c7af7de578db203f6dedaa7c86ef137a2d307
Daggerpov <danielagapov1@gmail.com>
2025-08-18 12:38:42 -0700

    rm userinfoservice for security for warning suppression

--- src/main/java/com/danielagapov/spawn/Config/SecurityConfig.java
+++ src/main/java/com/danielagapov/spawn/Config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.danielagapov.spawn.Config;
 
-import com.danielagapov.spawn.Services.UserDetails.UserInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,7 +29,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final UserInfoService userInfoService;
     private final JWTFilterConfig jwtFilterConfig;
     private final String[] whitelistedUrls = new String[] {
             "/api/v1/auth/refresh-token",
@@ -96,7 +92,6 @@ public class SecurityConfig {
                 // 'Stateless' session management means Spring will not create and store any session state on the server
                 // Each request is treated as 'new' and thus requires authentication (a JWT) to access secured endpoints
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtFilterConfig, UsernamePasswordAuthenticationFilter.class)
         ;
         return http.build();
@@ -118,21 +113,6 @@ public class SecurityConfig {
         return config.getAuthenticationManager(); // use default auth manager
     }
 
-    /**
-     * Configures the authentication provider for the application.
-     * <p>
-     * DaoAuthenticationProvider is used to authenticate users by retrieving user details
-     * from the database and validating their credentials.
-     *
-     * @return A configured DaoAuthenticationProvider instance.
-     */
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userInfoService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
 
     /**
      * Provides a password encoder bean.
